2b78f450caf7ee879199e3d6d5373af9b4cfed16
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-04-11 00:48:04 +0400

    Update gpt.py

--- userbot/modules/gpt.py
+++ userbot/modules/gpt.py
@@ -6,22 +6,54 @@ import random
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 
+from telethon import events
+from userbot import bot
+from g4f.client import Client
+from userbot.events import register
+
+client = Client()
+
 @register(outgoing=True, pattern=r"^\.gpt (.+)")
 async def gpt_plugin(event):
     mesaj = event.pattern_match.group(1)
     await event.edit("⏳ Cavab axtarılır...")
 
     try:
-        cavab = g4f.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            provider=g4f.Provider.Bing,
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
             messages=[{"role": "user", "content": mesaj}],
+            web_search=False
         )
-        await event.edit(f"**GPT Cavabı:**\n{cavab}")
+
+        gpt_response = response.choices[0].message.content
+        await event.edit(f"**GPT Cavabı:**\n{gpt_response}")
+
+    except Exception as e:
+        await event.edit(f"Xəta baş verdi:\n`{str(e)}`")
+
+
+@register(outgoing=True, pattern=r"^\.igpt (.+)")
+async def igpt_plugin(event):
+    mesaj = event.pattern_match.group(1)
+    await event.edit("⏳ Şəkil axtarılır...")
+
+    try:
+        image_response = client.images.generate(
+            model="flux",
+            prompt=mesaj,
+            response_format="url"
+        )
+
+        image_url = image_response.data[0].url
+        await event.edit("Şəkil tapıldı!")
+        await event.respond(f"**Generated Image:**", file=image_url)
+
     except Exception as e:
         await event.edit(f"Xəta baş verdi:\n`{str(e)}`")
 CmdHelp('chatgpt').add_command(
     'gpt', 'sual', 'ChatGPT nin UserBot versiyası məsələn .gpt SilgiUserbot niyə ən yaxşı userbotdur?'
+).add_command(
+    'igpt', 'şəkil', Yazdığınız şəkli yaradar'
 ).add_info(
     '⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝ Məhsuludur'
 ).add()
