10b5455d59faea8d1723872b920de00f36a3c166
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-04-11 00:12:56 +0400

    Update chatbot.py

--- userbot/modules/chatbot.py
+++ userbot/modules/chatbot.py
@@ -12,34 +12,34 @@ async def toggle_chatbot(event):
     chat_id = event.chat_id
 
     if cmd == "on":
-        db.activate_chat(chat_id)
+        db.aktiv_chat(chat_id)
         await event.edit("**ChatBot bu söhbət üçün aktiv edildi.**")
     elif cmd == "off":
-        db.deactivate_chat(chat_id)
+        db.deaktiv_chat(chat_id)
         await event.edit("**ChatBot bu söhbət üçün deaktiv edildi.**")
 
 @register(incoming=True)
 async def chatbot_main(event):
     chat_id = event.chat_id
     user_id = event.sender_id
-    if not db.is_chat_active(chat_id):
+    if not db.aktivdir(chat_id):
         return
 
-    message_text = event.text.strip()
+    mesaj = event.text.strip()
 
     if event.sender_id == (await bot.get_me()).id:
         return
 
-    db.add_user_message(user_id, message_text)
+    db.user_elave(user_id, mesaj)
 
-    answers = db.get_answers(message_text)
+    cavablar = db.cavab(mesaj)
     if answers:
         await event.reply(random.choice(answers))
     elif event.is_reply:
-        replied = await event.get_reply_message()
-        if replied and replied.sender_id != (await bot.get_me()).id:
-            original = replied.text.strip().lower()
-            db.add_pair(original, message_text)
+        cavablanan = await event.get_reply_message()
+        if cavablanan and cavablanan.sender_id != (await bot.get_me()).id:
+            orjinal = cavablanan.text.strip().lower()
+            db.elave_et(orjinal, mesaj)
 CmdHelp('chatbot').add_command(
     'chatbot', '<on/off>', 'Yazdığınız gruplarda ChatBot özəlliyini aktivləşdirər'
 ).add_info(
