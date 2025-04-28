96197d699b4929be4a14e9eab83bf5a07b8d3427
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-04-29 02:11:07 +0400

    Update config_lang.py

--- userbot/config_lang.py
+++ userbot/config_lang.py
@@ -1,5 +1,12 @@
 import os
+from logging import getLogger
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 
+# LOGS
+LOGS = getLogger(__name__)
+
+# LANGUAGE
 LANGUAGE = os.environ.get("LANGUAGE", None)
 
 if LANGUAGE:
@@ -8,5 +15,23 @@ else:
     LANGUAGE = "DEFAULT"
 
 if LANGUAGE not in ["EN", "TR", "AZ", "UZ", "DEFAULT"]:
-    print("[Dil]: Bilinməyən bir dil seçdiniz. Buna görə DEFAULT işlədilir.")
+    LOGS.info("[Dil]: Bilinməyən bir dil seçdiniz. Buna görə DEFAULT işlədilir.")
     LANGUAGE = "DEFAULT"
+
+# TELEGRAM BOT
+API_KEY = int(os.environ.get("API_ID", "0"))
+API_HASH = os.environ.get("API_HASH", None)
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
+
+if STRING_SESSION:
+    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+else:
+    bot = TelegramClient("userbot", API_KEY, API_HASH)
+
+# PLUGIN CHANNEL
+PLUGINID = os.environ.get("PLUGIN_CHANNEL_ID", None)
+
+if not PLUGINID:
+    PLUGIN_CHANNEL_ID = "me"
+else:
+    PLUGIN_CHANNEL_ID = int(PLUGINID)
