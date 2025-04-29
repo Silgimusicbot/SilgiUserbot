5f3724d017a034686d7907d529aa0a7fa5fe84b9
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-04-29 11:29:36 +0400

    Update asistant.py

--- userbot/modules/asistant.py
+++ userbot/modules/asistant.py
@@ -23,10 +23,11 @@ from telethon.events import callbackquery, InlineQuery, NewMessage
 from math import ceil
 import heroku3
 from userbot import bot, LOGS, HEROKU_APIKEY, HEROKU_APPNAME
-from userbot import CMD_HELP_BOT, PATTERNS
+from userbot import PATTERNS
 from userbot.language import get_value
 LANG = get_value("init")
 CMD_HELP={}
+CMD_HELP_BOT={}
 API_KEY = os.environ.get("API_KEY", None)
 API_HASH = os.environ.get("API_HASH", None)
 AVTO_Q = sb(os.environ.get("AVTO_Q", "True"))
