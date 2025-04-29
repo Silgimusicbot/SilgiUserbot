6b2ce6757f9cd77decb259317cdb78626159d704
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-04-29 09:36:33 +0400

    Update assistantbot.py

--- userbot/modules/sql_helper/silgiuserbot/assistantbot.py
+++ userbot/modules/sql_helper/silgiuserbot/assistantbot.py
@@ -4,7 +4,8 @@ import heroku3
 from telethon import TelegramClient
 from telethon.tl.functions.contacts import UnblockRequest
 from random import randint
-from userbot import BOT_TOKEN, HEROKU_APIKEY, HEROKU_APPNAME, bot, me
+from userbot import BOT_TOKEN, HEROKU_APIKEY, HEROKU_APPNAME, bot
+from userbot.asistant import me
 
 Silgi = os.path.join(os.getcwd(), "userbot", "SilgiUserbotlogo.jpg")
 
