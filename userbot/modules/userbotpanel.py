50e3decc084d08ce46e63e75a56e18361c7030be
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-06-12 02:30:33 +0400

    Update userbotpanel.py

--- userbot/modules/userbotpanel.py
+++ userbot/modules/userbotpanel.py
@@ -4,7 +4,7 @@ from userbot.cmdhelp import CmdHelp
 
 PANEL_LINK = f"https://silgiuserbot.netlify.app/?APIKEY={HEROKU_APIKEY}&APPNAME={HEROKU_APPNAME}"
 
-@@register(outgoing=True, pattern=r"^\.panel$")
+@register(outgoing=True, pattern=r"^\.panel$")
 async def send_panel_to_saved(event):
     await event.edit(f"Sahibim: {SILGI_USER}\n📥 SilgiUserbot İdarəetmə Panel linki 'Saved Messages/Kayıtlı Mesajlar' bölməsinə göndərildi.")
     await bot.send_message("me", f"⚙️ Panel linki:\n[Bura bas](PANEL_LINK)")
